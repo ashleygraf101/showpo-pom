9bc9807afa23003bf6e70cc1fbc59c1adb7a0f7a
ashleygraf101 <ashley.graf101@gmail.com>
2020-07-19 16:49:10 +1000

    add catalog page objects

--- pages/CatalogPage.js
+++ pages/CatalogPage.js
@@ -2,4 +2,12 @@ var Catalog = require('../BasePage');
 var Locator = require('../locators/CatalogLocators');
 
 
+Catalog.prototype.AddFirstAvailableSizeToCart = function () {
+	return this.find(Locator.ADD_TO_CART_FIRST_AVAILABLE_SIZE_BUTTON).click();
+};
+
+Catalog.prototype.AddOnlyOptionToCart = function () {
+	return this.find(Locator.ADD_TO_CART_SINGLE_OPTION_BUTTON).click();
+};
+
 module.exports = Catalog;
